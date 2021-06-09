1485cbf65c23b31d44f9ce083dd99ebc9c5136ec
whycardboard <49077192+whycardboard@users.noreply.github.com>
2021-06-09 09:24:30 -0600

    finished i hope

--- src/About.js
+++ src/About.js
@@ -6,15 +6,21 @@ export const About = () => (
         <br />
         <h3>Daisy's dress</h3>
         <p>Throughout the story of The Great Gatsby, Daisy's clothing is symbolic of not only her state in society, but our perception of her in the story.<br /><br />
-            In the beginning of the story, we are introduced to Daisy in a beautiiful white dress, "They were both in white" (p.13). This white dress is a symbol of purity and innocence.
+            In the beginning of the story, we are introduced to Daisy in a beautiful white dress, "They were both in white" (p.13). This white dress is a symbol of purity and innocence.
             Daisy is percieved to be a perfect, pure of heart housewife, in a happy stable relationship with Tom Buchanan.<br /><br />
             Near the end of the story, we can see that Daisy's dress has turned to a shade of yellow. This transition is representative of the reader learning and understanding all of the flaws and facets of Daisy.
-            This yellow dress is symbolic of not only infidelity, which we've seen Daisy commit with Gatsby and Tom, but also deceit. Daisy's motive for her scandals with Tom and Gatsby was for the most part, a means to gain wealth, and economic status.
+            This yellow dress is symbolic of not only infidelity, which we've seen Daisy commit with Gatsby and Tom, but also deceit. Daisy's motive for her scandals with Tom and Gatsby was for the most part, a means to gain wealth, and economic status.<br /><br />
+            As we explained in our theme statement, Daisy is a representation of the failed American Dream. Although our dreams may start in a pure, lighthearted state, inevetably they will begin to taint yellow as our motives change. Daisy's motive throughout the story is economic gain, and the accumulation of money; the same concept that the American Dream enforces. 
         </p>
         <h3>Daisy's heart</h3>
-        <p>Daisy's heart switches between both Tom and Gatsby throughout the story. </p>  
+        <p>Daisy's heart is not only a symbol of where her romantic interests lie, but her financial interests as well.<br /><br />
+        As we see throughout the story, Daisy's love interest switches between Tom and Gatsby, and by association, her personality switches as well. Daisy's economic status comes from associoation with men she is seeing. Throughout the story, we can see her personality shift between more refined and prestigious, with Tom and the East Egg, to more fun-loving and care-free, Gatsby and the West Egg. Her heart is a constant representation of where her loyalties lie, and where her economic status exists.<br />< br />
+        This idea is also portrayed in the American Dream. Holding a single job with a single stream of income is not financially feasable, and people who wish to gain economic status need to play all the angles. Not only were scandals traditional in the setting of the book, but also the 1920's themselves. Economic gain is always at the forefront of the American Dream.</p>  
         <h3>Daisy's voice</h3>
-        <p>placeholder</p>  
+        <p>Daisy's voice is the one symbol that stays consistent throughout the story: her money.<br /><br />
+        Daisy has always had an association with money, and the means of which she gains money. In chapter 7, Gatsby reveals the thing that was nagging Nick throughout the story: Daisy's voice was full of money. This constant reminder that Daisy's ideals remain constant through the entire story. In every chapter, there is a representation of Daisy's voice, always full of richness and wealth.<br /><br />
+        The American Dream has always been a scramble for money, and Daisy's goals have remained the same through the entire novel.
+        </p>  
 
     </div>
 );
--- src/Game.js
+++ src/Game.js
@@ -44,51 +44,44 @@ class Game extends React.Component {
             time: 0,
             questions: [
                 [
-                    [<h2>In F. Scott Fitzgerald's book <i>The Great Gatsby</i>, Daisy says...</h2>],
+                    [<h3>In F. Scott Fitzgerald's book <i>The Great Gatsby</i>, Daisy says...</h3>],
                     ["\"'The West Egg is my new home', Daisy states.\" (Fitzgerald, p. 35)", 0],
                     ["\"'What if I'm not a hero. What if I'm the bad guy?' Edward cries.\" (Meyer, p. 221)", 0],
                     ["\"'They're such beautiful shirts,' she sobbed, her voice muffled in the thick folds. 'It makes me sad because I've never seen such-such beautiful shirts before.'\" (Fitzgerald, p. 92)", 1],
                     ["\"'There's Myrtle!', Daisy exclaims as she steps on the gas.\" (Fitzgerald, p. 166)", 0]
                 ],
                 [
-                    [<h2>"'They're such beautiful shirts,' she sobbed, her voice muffled in the thick folds. 'It makes me sad because I've never seen such-such beautiful shirts before.'” represents Daisy's symbol of...</h2>],
+                    [<h3>"'They're such beautiful shirts,' she sobbed, her voice muffled in the thick folds. 'It makes me sad because I've never seen such-such beautiful shirts before.'” represents Daisy's symbol of...</h3>],
                     ["Her heart", 1],
                     ["Her voice", 0],
                     ["Her dress", 0],
                 ],
                 [
-                    [<h2>In F. Scott Fitzgerald's book <i>The Great Gatsby</i>, this quote reveals a symbol of Daisy.</h2>],
+                    [<h3>In F. Scott Fitzgerald's book <i>The Great Gatsby</i>, this quote reveals a symbol of Daisy.</h3>],
                     ["\"That was the inexhaustible charm that rose and fell in it, the jingle of it, the cymbals' song of it.\" (Fitzgerald, p. 105)", 1],
                     ["\"'Real macaroni and cheese doesn't come from a box, babe. It eventually comes from an oven with a crust bubbling on top'\" (Noel, p. 601)", 0],
                     ["\"'My dress! Oh god, my dress! It's stained! And right before the party!'\" (Fitzgerald, p. 26)", 0],
                     ["\"Maybe she just wanted to wear something different this time when meeting Gatsby.\" (Lazaryev, p. 1)", 0]
                 ],
                 [
-                    [<h2>"That was the inexhaustible charm that rose and fell in it, the jingle of it, the cymbals' song of it.” represents Daisy's symbol of...</h2>],
+                    [<h3>"That was the inexhaustible charm that rose and fell in it, the jingle of it, the cymbals' song of it.” represents Daisy's symbol of...</h3>],
                     ["Her heart", 0],
                     ["Her voice", 1],
                     ["Her dress", 0],
                 ],
                 [
-                    [<h2>In F. Scott Fitzgerald's book <i>The Great Gatsby</i>, this quotes introduces a symbol of Daisy.</h2>],
+                    [<h3>In F. Scott Fitzgerald's book <i>The Great Gatsby</i>, this quotes introduces a symbol of Daisy.</h3>],
                     ["\"You know you're in love when you can't fall asleep because reality is finally better than your dreams.\" (Seuss, p. 123)", 0],
                     ["\"'Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.'\" (Fitzgerald, p. 4)", 0],
                     ["\"'I don't want to repeat my innocence. I want the pleasure of losing it again.'\" (Fitzgerald, p. 184)", 0],
                     ["\"They were both in white and their dresses were rippling and fluttering.\" (Fitzgerald, p. 2)", 1]
                 ],
                 [
-                    [<h2>"They were both in white and their dresses were rippling and fluttering.” represents Daisy's symbol of...</h2>],
+                    [<h3>"They were both in white and their dresses were rippling and fluttering.” represents Daisy's symbol of...</h3>],
                     ["Her heart", 0],
                     ["Her voice", 0],
                     ["Her dress", 1],
-                ],
-                [
-                    [<h2>In F. Scott Fitzgerald's book <i>The Great Gatsby</i>, this quotes introduces a symbol of Daisy.</h2>],
-                    ["\"You know you're in love when you can't fall asleep because reality is finally better than your dreams.\" (Seuss, p. 123)", 0],
-                    ["\"'Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.'\" (Fitzgerald, p. 4)", 0],
-                    ["\"'I don't want to repeat my innocence. I want the pleasure of losing it again.'\" (Fitzgerald, p. 184)", 0],
-                    ["\"They were both in white and their dresses were rippling and fluttering.\" (Fitzgerald, p. 2)", 1]
-                ],
+                ]
             ],
             current: 0,
             over: false
@@ -111,15 +104,15 @@ class Game extends React.Component {
 
     click(isCorrect) {
         if(isCorrect === 1) {
-            alert("yay!");
+            alert("Correct!");
             if(this.state.current+1 >= this.state.questions.length) {
-                alert("you win!");
+                alert("Congrats! You win!");
                 this.setState({time: this.state.time, questions: this.state.questions, current: this.state.current+1, over: true});
             } else {
                 this.setState({time: this.state.time, questions: this.state.questions, current: this.state.current+1, over: false});
             }
         } else {
-            alert("you suck!");
+            alert("Wrong! Try again.");
 
         }
     }
